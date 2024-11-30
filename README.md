a16ee943dcedabfa6416139963d403e6c1cd6a34
Erik Pohl <epohl@dmigs.com>
2024-11-30 18:24:55 -0500

    added some create statements

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 To do:
-- [x] Create a warehouse
+- [x] Create a warehouse using web UI
 - [x] Load table with data from local file using Snowflake web page
 - [x] Load table with data from s3 bucket using SQL script in Snowflake website
 - [x] Load table with data from s3 bucket using python script in Snowflake website
 - [x] Read data into local machine into a pandas dataframe using python script from warehouse
+- [ ] Create warehouse, db, schema from python connection
--- main.py
+++ main.py
@@ -21,14 +21,15 @@ def connect_to_mystuff():
 
 
 def request_data(sfconn, query):
-
-    try:
-        with sfconn.cursor() as cursor:
-            cursor.execute(query)
-            data = cursor.fetch_pandas_all()
-    finally:
-        sfconn.close()
+    with sfconn.cursor() as cursor:
+        cursor.execute(query)
+        data = cursor.fetch_pandas_all()
     return data
+
+def create_sf_warehouse(sf_conn, sf_object_type, sf_object_name):
+    result = sf_conn.cursor().execute(f"CREATE {sf_object_type} IF NOT EXISTS {sf_object_name}")
+    print(f'***{result}')
+    return True
 
 
 if __name__ == '__main__':
@@ -43,3 +44,16 @@ if __name__ == '__main__':
              "WHERE FIRST_NAME like 'S%' and sex not in ('Female');")
     sales = request_data(sfconn, query)
     print(sales)
+    create_sf_warehouse(sfconn, "WAREHOUSE", "my_new_warehouse_ep")
+    create_sf_warehouse(sfconn, "DATABASE", "my_new_database_ep")
+    sfconn.cursor().execute("USE DATABASE my_new_database_ep")
+    create_sf_warehouse(sfconn, "SCHEMA", "my_new_schema_ep")
+    sfconn.cursor().execute(
+        "CREATE OR REPLACE TABLE "
+        "test_table(col1 integer, col2 string)")
+    sfconn.cursor().execute(
+        "INSERT INTO test_table(col1, col2) VALUES " +
+        "    (123, 'test string1'), " +
+        "    (456, 'test string2')")
+    sfconn.cursor().close()
+
